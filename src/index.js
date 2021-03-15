b01af71b7690af05fac6b0a8343c0e8c84ffd168
TetyanaRedka <76970722+TetyanaRedka@users.noreply.github.com>
2021-03-15 21:45:12 +0200

    дз

---
+++ src/index.js
@@ -0,0 +1,47 @@
+import debounce from 'lodash.debounce';
+// var debounce = require('lodash.debounce');
+// import { debounce } from 'lodash';
+import './styles.css';
+import countryone from './countryone.hbs';
+import countriesCard from './countries.hbs';
+
+import { alert, error, defaultModules } from '@pnotify/core/dist/PNotify.js';
+import '@pnotify/core/dist/PNotify.css';
+import '@pnotify/core/dist/BrightTheme.css';
+
+const inputEll = document.querySelector('#request');
+const resultEll = document.querySelector('.result');
+const getCountry = debounce(function () {
+  getCountries();
+}, 1000);
+
+inputEll.addEventListener('input', getCountry);
+
+function getCountries() {
+  fetchCountrys().then(renderCountry).catch(errorCountry);
+}
+
+function fetchCountrys() {
+  return fetch(`https://restcountries.eu/rest/v2/name/${inputEll.value}`).then(
+    response => {
+      return response.json();
+    },
+  );
+}
+
+function renderCountry(countries) {
+  if (countries.length === 1) {
+    return (resultEll.innerHTML = countryone(countries));
+  } else if (countries.length <= 10) {
+    return (resultEll.innerHTML = countriesCard(countries));
+  }
+  error({
+    text: 'Too many matches found. Please enter a more specific query!',
+  });
+}
+
+function errorCountry() {
+  alert({
+    text: 'Сountry with such a set was not found!',
+  });
+}
